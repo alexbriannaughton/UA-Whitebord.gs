29e277a73e72b2af78a8ecafe68561edc72ed896
Alex Brian Naughton <alexbriannaughton@gmail.com>
2024-01-16 16:30:50 -0800

    unknown

--- src/Helpers.js
+++ src/Helpers.js
@@ -161,7 +161,7 @@ function getAnimalInfoAndLastName(animalID, contactID) {
   const parsedAnimal = JSON.parse(animalJSON);
   const animal = parsedAnimal.items.at(-1).animal;
   const speciesMap = { 1: 'K9', 2: 'FEL' };
-  const animalSpecies = speciesMap[animal.species_id] || '';
+  const animalSpecies = speciesMap[animal.species_id] || 'Unknown species';
 
   const contactJSON = contactResponse.getContentText();
   const parsedContact = JSON.parse(contactJSON);
