33d78937aa8f37b11bca0f6d757464b68576d1b4
Alex Brian Naughton <alexbriannaughton@gmail.com>
2024-03-16 13:12:28 -0700

    dev

--- src/Date.js
+++ src/Date.js
@@ -30,3 +30,13 @@ function convertEpochToSeattleTime(epoch) {
     const date = new Date(epoch * 1000);
     return Utilities.formatDate(date, 'America/Los_Angeles', 'hh:mm a');
 };
+
+function epochRangeForTomorrow() {
+    // get epochs for range of tomorrow
+  const now = new Date().toLocaleString("en-US", { timeZone: "America/Los_Angeles" });
+  const tomorrow = new Date(now);
+  tomorrow.setDate(tomorrow.getDate() + 2); // Move to tomorrow
+  const tomorrowStart = Math.floor(tomorrow.setHours(0, 0, 0, 0) / 1000); // midnight tomorrow in seconds
+  const tomorrowEnd = Math.floor(tomorrow.setHours(23, 59, 59, 999) / 1000); // end of tomorrow in seconds
+  return [tomorrowStart, tomorrowEnd];
+}
--- src/TomorrowsDTAppts.js
+++ src/TomorrowsDTAppts.js
@@ -1,7 +1,7 @@
 function downloadPdfToDrive() {
   const dlLink = 'https://urbananimalnw.usw2.ezyvet.com/api/v1/attachment/download/4425719970';
   const cache = CacheService.getScriptCache();
-  let token = cache.get('ezyVet_token');
+  const token = cache.get('ezyVet_token');
 
   if (!token) {
     token = PropertiesService.getScriptProperties().getProperty('ezyVet_token');
@@ -37,37 +37,17 @@ function downloadPdfToDrive() {
 }
 
 function getTomorrowsDTAppts() {
-  // get epochs for range of tomorrow
-  const now = new Date().toLocaleString("en-US", { timeZone: "America/Los_Angeles" });
-  const tomorrow = new Date(now);
-  tomorrow.setDate(tomorrow.getDate() + 2); // Move to tomorrow
-  const tomorrowStart = Math.floor(tomorrow.setHours(0, 0, 0, 0) / 1000); // midnight tomorrow in seconds
-  const tomorrowEnd = Math.floor(tomorrow.setHours(23, 59, 59, 999) / 1000); // end of tomorrow in seconds
+  const [tomorrowStart, tomorrowEnd] = epochRangeForTomorrow();
   // send query for all appointments for tomorrow
   const url = `${proxy}/v1/appointment?active=1&time_range_start=${tomorrowStart}&time_range_end=${tomorrowEnd}&limit=200`;
   const allOfTomorrowsAppts = fetchAndParse(url);
-
-  // filter all appts down to DT exams and techs
-  const dtResourceIDs = new Set([ // non procedures dt columns
-    '35', // dt dvm 1
-    '55', // used to be dt dvm 2, though it is not currently active 3/16/24
-    '56', // dt tech
-    '1015', // used to be dt dvm 3, though it is not currently active 3/16/24
-    '1082' // dt DVM :15/:45
-  ]);
-  const dtAppts = allOfTomorrowsAppts.items.filter(({ appointment }) => {
-    return appointment.details.resource_list.some(id => dtResourceIDs.has(id)) // is in DT exam or tech column
-      && appointment.details.appointment_type_id !== '4'; // is not a blocked off spot
-  });
-
-  dtAppts.sort((a, b) => a.appointment.start_time - b.appointment.start_time);
-
-  const firstTwoApptsForTesting = dtAppts.slice(0, 2); // for prod we will use dtAppts instead
-  getAllEzyVetData(firstTwoApptsForTesting);
+  const dtAppts = filterAndSortDTAppts(allOfTomorrowsAppts);
+  getAllEzyVetData(dtAppts);
 
   const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('DT Next Day Checklist');
   const range = sheet.getRange(`A4:C204`)
   range.clearContent();
+  range.setWrap(true);
 
   for (let i = 0; i < dtAppts.length; i++) {
     if (!dtAppts[i].contact) break; // this is just for dev, shouldnt be necessary in prod
@@ -86,7 +66,7 @@ function getTomorrowsDTAppts() {
 
     const ptCell = range.offset(i, 1, 1, 1);
     const ptSpecies = speciesMap[animal.species_id] || 'unknown species';
-    const ptText = `${animal.name} ${contact.lastName} (${ptSpecies})`;
+    const ptText = `${animal.name} ${contact.last_name} (${ptSpecies})`;
     const animalURL = `${sitePrefix}/?recordclass=Animal&recordid=${animal.id}`;
     const link = makeLink(ptText, animalURL);
     ptCell.setRichTextValue(link);
@@ -94,20 +74,41 @@ function getTomorrowsDTAppts() {
     const reasonCell = range.offset(i, 2, 1, 1);
     let descriptionString = appointment.details.description;
     if (descriptionString.startsWith('VETSTORIA')) {
-      const itemsInParenthases = descriptionString.match(/\((.*?)\)/g);
-      const reason = itemsInParenthases.at(-1);
-      descriptionString = reason.slice(1, -1);
+      const itemsInParentheses = descriptionString.match(/\((.*?)\)/g);
+      const lastItem = itemsInParentheses.at(-1);
+      descriptionString = lastItem.slice(1, -1); // remove parentheses
     }
     reasonCell.setValue(descriptionString);
+
+    const recordsCell = range.offset(i, 4, 1, 1);
+
   }
+};
+
+function filterAndSortDTAppts(allOfTomorrowsAppts) {
+  // filter all appts down to DT exams and techs
+  const dtResourceIDs = new Set([ // non procedures dt columns
+    '35', // dt dvm 1
+    '55', // used to be dt dvm 2, though it is not currently active 3/16/24
+    '56', // dt tech
+    '1015', // used to be dt dvm 3, though it is not currently active 3/16/24
+    '1082' // dt DVM :15/:45
+  ]);
+  const dtAppts = allOfTomorrowsAppts.items.filter(({ appointment }) => {
+    return appointment.details.resource_list.some(id => dtResourceIDs.has(id)) // is in DT exam or tech column
+      && appointment.details.appointment_type_id !== '4'; // is not a blocked off spot
+  });
+
+  return dtAppts.sort((a, b) => a.appointment.start_time - b.appointment.start_time)
+    .slice(0, 2); // for dev we are just slicing the first two
 }
 
 // get the animal, contact and attachment data associated with the appointment
 function getAllEzyVetData(dtAppts) {
-  let animalRequests = [];
-  let contactRequests = [];
-  let animalAttachmentRequests = [];
-  let allConsultsForAnimalRequests = [];
+  const animalRequests = [];
+  const contactRequests = [];
+  const animalAttachmentRequests = [];
+  const allConsultsForAnimalRequests = [];
   dtAppts.forEach(({ appointment }) => {
     animalRequests.push({
       muteHttpExceptions: true,
@@ -135,28 +136,53 @@ function getAllEzyVetData(dtAppts) {
     })
   });
 
-  let animalResponses = UrlFetchApp.fetchAll(animalRequests);
-  let contactResponses = UrlFetchApp.fetchAll(contactRequests);
-  let animalAttachmentResponses = UrlFetchApp.fetchAll(animalAttachmentRequests);
-  let allConsultsForAnimalResponses = UrlFetchApp.fetchAll(allConsultsForAnimalRequests);
+  const animalResponses = UrlFetchApp.fetchAll(animalRequests);
+  const contactResponses = UrlFetchApp.fetchAll(contactRequests);
+  const animalAttachmentResponses = UrlFetchApp.fetchAll(animalAttachmentRequests);
+  const allConsultsForAnimalResponses = UrlFetchApp.fetchAll(allConsultsForAnimalRequests);
 
   animalResponses.forEach((response, i) => {
-    let animal = JSON.parse(response.getContentText()).items.at(-1).animal;
+    const animal = JSON.parse(response.getContentText()).items.at(-1).animal;
     dtAppts[i].animal = animal;
   });
   contactResponses.forEach((response, i) => {
-    let contact = JSON.parse(response.getContentText()).items.at(-1).contact;
+    const contact = JSON.parse(response.getContentText()).items.at(-1).contact;
     dtAppts[i].contact = contact;
   });
+  const animalAttachmentDownloadRequests = [];
   animalAttachmentResponses.forEach((response, i) => {
-    let attachments = JSON.parse(response.getContentText()).items;
-    dtAppts[i].animalAttachments = attachments;
+    const animalAttachments = JSON.parse(response.getContentText()).items;
+    // download all of these into drive
+    animalAttachments.forEach(({ attachment }) => {
+      animalAttachmentDownloadRequests.push({
+        muteHttpExceptions: true,
+        url: `${attachment.file_download_url}`,
+        method: "GET",
+        headers: { authorization: token }
+      })
+    })
+    // save the id numbers for the button that will open the drive files
+    dtAppts[i].animalAttachments = animalAttachments;
   });
   allConsultsForAnimalResponses.forEach((response, i) => {
-    let consults = JSON.parse(response.getContentText()).items;
+    const consults = JSON.parse(response.getContentText()).items;
     const consultIDs = consults.map(({ consult }) => consult.id);
     dtAppts[i].consultIDs = consultIDs;
   });
+
+  const animalAttachmentDownloadResponses = UrlFetchApp.fetchAll(animalAttachmentDownloadRequests);
+  animalAttachmentDownloadResponses.forEach(response => {
+    const blob = response.getBlob();
+    const fileName = blob.getName();
+    const existingFiles = DriveApp.getFilesByName(fileName); // returns FileIterator object
+    const file = existingFiles.hasNext() // if the file exists
+      ? existingFiles.next() // use it
+      : DriveApp.createFile(blob); // otherwise create it in Drive, and use that
+
+    // const fileUrl = file.getUrl();
+    // const html = HtmlService.createHtmlOutput('<script>window.open("' + fileUrl + '");</script>');
+    // SpreadsheetApp.getUi().showModalDialog(html, 'Opening PDF...');
+  })
 
   const consultAttachmentRequests = [];
   for (const appt of dtAppts) {
