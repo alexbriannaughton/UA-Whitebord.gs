fce210c1e674b9275e7681f320859e52320088ca
Alex Brian Naughton <alexbriannaughton@gmail.com>
2024-03-19 16:37:03 -0700

    all rooms

--- src/extractRooms.js
+++ src/extractRooms.js
@@ -1,15 +1,12 @@
 function extractRooms(sheetName, rangeCoords, indexToSatusIDMap, allRooms) {
     const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sheetName);
     const range = sheet.getRange(rangeCoords);
-    const vals = range.getValues();
     const rtVals = range.getRichTextValues();
-    const rowFourVals = vals[0];
     const rowFourRTVals = rtVals[0];
 
-    parseOneRow(rowFourVals, rowFourRTVals, indexToSatusIDMap, allRooms, sheetName);
+    parseOneRow(rowFourRTVals, indexToSatusIDMap, allRooms, sheetName);
 
     if (sheetName === 'CH') {
-        const rowFourteenVals = vals.at(-1);
         const rowFourteenRTVals = rtVals.at(-1);
         const chRowFourteenIndexToSatusIDMap = new Map([
             [0, '29'], // room 6
@@ -20,30 +17,26 @@ function extractRooms(sheetName, rangeCoords, indexToSatusIDMap, allRooms) {
             [5, '36'], //Room 11
             [6, '39'] //Dog Lobby
         ]);
-        parseOneRow(rowFourteenVals, rowFourteenRTVals, chRowFourteenIndexToSatusIDMap, allRooms, sheetName);
+        parseOneRow(rowFourteenRTVals, chRowFourteenIndexToSatusIDMap, allRooms, sheetName);
     }
 
     return allRooms;
 }
 
-function parseOneRow(rowVals, rowRTVals, indexToSatusIDMap, allRooms, sheetName) {
-    for (let i = 0; i < rowVals.length; i++) {
+function parseOneRow(rowRTVals, indexToSatusIDMap, allRooms, sheetName) {
+    for (let i = 0; i < rowRTVals.length; i++) {
         const statusID = indexToSatusIDMap.get(i);
-        const val = rowVals[i];
         const richText = rowRTVals[i];
-        const roomDetails = { val };
-
         const runs = richText.getRuns();
         for (const richText of runs) {
             const link = richText.getLinkUrl();
             if (link?.includes('Consult')) {
-                roomDetails.consultID = link.split('=')[2];
+                const consultID = link.split('=')[2];
+                const roomLocationKey = sheetName + statusID;
+                allRooms[roomLocationKey] = consultID;
                 break;
             }
         }
-
-        const roomLocationKey = sheetName + statusID;
-        allRooms[roomLocationKey] = roomDetails;
 
     }
 
