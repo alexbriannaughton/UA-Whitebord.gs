c7f62bd2cfd03e7fa149e7e2d627a5b2d6a844b3
Alex Brian Naughton <alexbriannaughton@gmail.com>
2024-03-19 14:43:54 -0700

    all rooms

--- src/Code.js
+++ src/Code.js
@@ -105,115 +105,6 @@ function handleUpdatedAppointment(appointment) {
 function doGet(_e) {
   const allRooms = {};
 
-  // const chRooms = {}
-  // const chSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('CH');
-  // const chRange = chSheet.getRange('C4:I14');
-  // const chVals = chRange.getValues();
-  // const chRTVals = chRange.getRichTextValues();
-
-  // const chRowFourVals = chVals[0];
-  // const chRowFourRTVals = chRTVals[0];
-  // const chRowFourIndexToRoomNameMap = new Map([
-  //   [0, 'Room 1'],
-  //   [1, 'Room 2'],
-  //   [2, 'Room 3'],
-  //   [3, 'Room 4'],
-  //   [4, 'Room 5'],
-  //   [5, 'Cat Lobby 1'],
-  //   [6, 'Cat Lobby 2']
-  // ]);
-
-  // for (let i = 0; i < 7; i++) {
-  //   const roomName = chRowFourIndexToRoomNameMap.get(i);
-  //   const val = chRowFourVals[i];
-  //   const richText = chRowFourRTVals[i];
-  //   const roomDetails = { val };
-
-  //   const runs = richText.getRuns();
-  //   for (const richText of runs) {
-  //     const link = richText.getLinkUrl();
-  //     if (link?.includes('Consult')) {
-  //       roomDetails.consultID = link.split('=')[2];
-  //       break;
-  //     }
-  //   }
-
-  //   chRooms[roomName] = roomDetails;
-  // }
-
-  // const chRowFourteenVals = chVals.at(-1);
-  // const chRowFourteenRTVals = chRTVals.at(-1);
-  // const chRowFourteenIndexToRoomNameMap = new Map([
-  //   [0, 'Room 6'],
-  //   [1, 'Room 7'],
-  //   [2, 'Room 8'],
-  //   [3, 'Room 9'],
-  //   [4, 'Room 10'],
-  //   [5, 'Room 11'],
-  //   [6, 'Dog Lobby']
-  // ]);
-
-  // for (let i = 0; i < 7; i++) {
-  //   const roomName = chRowFourteenIndexToRoomNameMap.get(i);
-  //   const val = chRowFourteenVals[i];
-  //   const richText = chRowFourteenRTVals[i];
-  //   const roomDetails = { val };
-
-  //   const runs = richText.getRuns();
-  //   for (const richText of runs) {
-  //     const link = richText.getLinkUrl();
-  //     if (link?.includes('Consult')) {
-  //       roomDetails.consultID = link.split('=')[2];
-  //       break;
-  //     }
-  //   }
-
-  //   chRooms[roomName] = roomDetails;
-  // }
-
-  // allRooms.CH = chRooms;
-
-
-
-  // const dtRooms = {};
-  // const dtSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('DT');
-  // const dtRange = dtSheet.getRange('C4:I4');
-  // const dtVals = dtRange.getValues();
-  // const dtRTVals = dtRange.getRichTextValues();
-
-  // const dtRowFourVals = dtVals[0];
-  // const dtRowFourRTVals = dtRTVals[0];
-  // const dtRowFourIndexToRoomNameMap = new Map([
-  //   [0, 'Room 1'],
-  //   [1, 'Room 2'],
-  //   [2, 'Room 3'],
-  //   [3, 'Room 4'],
-  //   [4, 'Room 5'],
-  //   [5, 'Room 6'],
-  //   [6, 'Room 7']
-  // ]);
-
-  // for (let i = 0; i < 7; i++) {
-  //   const roomName = dtRowFourIndexToRoomNameMap.get(i);
-  //   const val = dtRowFourVals[i];
-  //   const richText = dtRowFourRTVals[i];
-  //   const roomDetails = { val };
-
-  //   const runs = richText.getRuns();
-  //   for (const richText of runs) {
-  //     const link = richText.getLinkUrl();
-  //     if (link?.includes('Consult')) {
-  //       roomDetails.consultID = link.split('=')[2];
-  //       break;
-  //     }
-  //   }
-
-  //   dtRooms[roomName] = roomDetails;
-  // }
-
-  // allRooms.DT = dtRooms;
-
-
   const chRowFourIndexToRoomNameMap = new Map([
     [0, 'Room 1'],
     [1, 'Room 2'],
@@ -224,7 +115,8 @@ function doGet(_e) {
     [6, 'Cat Lobby 2']
   ]);
 
-  const dtRowFourIndexToRoomNameMap = new Map([
+  // this map works for both WC and DT, even though WC only has 5 rooms
+  const rowFourIndexToRoomNameMap = new Map([
     [0, 'Room 1'],
     [1, 'Room 2'],
     [2, 'Room 3'],
@@ -234,17 +126,9 @@ function doGet(_e) {
     [6, 'Room 7']
   ]);
 
-  const wcRowFourIndexToRoomNameMap = new Map([
-    [0, 'Room 1'],
-    [1, 'Room 2'],
-    [2, 'Room 3'],
-    [3, 'Room 4'],
-    [4, 'Room 5']
-  ]);
-
   allRooms.CH = extractRooms('CH', 'C4:I14', chRowFourIndexToRoomNameMap);
-  allRooms.DT = extractRooms('DT', 'C4:I4', dtRowFourIndexToRoomNameMap);
-  allRooms.WC = extractRooms('WC', 'C4:G4', wcRowFourIndexToRoomNameMap);
+  allRooms.DT = extractRooms('DT', 'C4:I4', rowFourIndexToRoomNameMap);
+  allRooms.WC = extractRooms('WC', 'C4:G4', rowFourIndexToRoomNameMap);
 
   return ContentService.createTextOutput(
     JSON.stringify(allRooms)
