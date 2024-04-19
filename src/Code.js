fd9a5e7d3b21242ea177d137ba3ed6e26c0fbfc1
Alex Brian Naughton <alexbriannaughton@gmail.com>
2024-04-18 17:01:23 -0700

    test2

--- src/Code.js
+++ src/Code.js
@@ -1,6 +1,6 @@
 // receive appointment webhook events here
 function doPost(e) {
-  console.log('new gcp project test');
+  console.log('new gcp project test1');
   try {
     const startTime = new Date(); // this is for logging executions that hang exessively 
     const params = JSON.parse(e.postData.contents);
