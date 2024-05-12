fad8ad733c072b1af8789d421222c172358ff4e1
Alex Brian Naughton <alexbriannaughton@gmail.com>
2024-05-12 14:09:54 -0700

    change format wait data

--- src/Cron-Jobs/WaitData.js
+++ src/Cron-Jobs/WaitData.js
@@ -10,13 +10,22 @@ function getWaitData() {
 function getWaitValsForLocation(ssApp, location) {
     const waitlistSheet = ssApp.getSheetByName(`${location} Wait List`);
     const vals = waitlistSheet.getRange('C2:D4').getValues();
+    const capText = vals[0][1];
+    const { soft_cap, hard_cap } = checkForCap(capText);
     return {
         location,
+        soft_cap,
+        hard_cap,
         num_of_dvms_on_floor: vals[1][0],
         wb_wait_time: vals[2][0],
-        num_of_pts_waiting: vals[0][0],
-        capText: vals[0][1]
+        num_of_pts_waiting: vals[0][0]
     };
+}
+
+function checkForCap(capText) {
+    const soft_cap = capText === 'Cancellation List Only';
+    const hard_cap = capText.includes('Not Currently Accepting Walk-ins');
+    return { soft_cap, hard_cap };
 }
 
 // function sendWaitData(waitData) {
