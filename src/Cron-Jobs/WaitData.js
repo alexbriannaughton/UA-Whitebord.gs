e4df5456459d07492041b10ab46b7699c764b57f
Alex Brian Naughton <alexbriannaughton@gmail.com>
2024-05-12 13:41:41 -0700

    wait vals 2 locs

--- src/Cron-Jobs/WaitData.js
+++ src/Cron-Jobs/WaitData.js
@@ -1,26 +1,31 @@
 function getWaitData() {
     const ssApp = SpreadsheetApp.getActiveSpreadsheet();
-    const chWaitlistSheet = ssApp.getSheetByName('CH Wait List');
-    const chRange = chWaitlistSheet.getRange('C2:D4');
-    const chVals = chRange.getValues();
-    const waitData = {}
-    waitData.ch = {
-        ptsOnWaitlist: chVals[0][0],
-        dvmsOnFloor: chVals[1][0],
-        estWait: chVals[2][0],
-        capText: chVals[0][1]
+    const waitData = {
+        ch: getWaitValsForLocation(ssApp, 'CH Wait List'),
+        wc: getWaitValsForLocation(ssApp, 'WC Wait List')
     };
     return waitData;
 }
 
-function sendWaitData(waitData) {
-    const url = PropertiesService.getScriptProperties().getProperty('wait_tracker_url');
-    const options = {
-        method: "post",
-        contentType: "application/json",
-        payload: JSON.stringify(waitData)
+function getWaitValsForLocation(ssApp, sheetName) {
+    const waitlistSheet = ssApp.getSheetByName(sheetName);
+    const vals = waitlistSheet.getRange('C2:D4').getValues();
+    return {
+        ptsOnWaitlist: vals[0][0],
+        dvmsOnFloor: vals[1][0],
+        estWait: vals[2][0],
+        capText: vals[0][1]
     };
-    const response = UrlFetchApp.fetch(url, options);
-    const content = response.getContentText();
-    console.log(content);
 }
+
+// function sendWaitData(waitData) {
+//     const url = PropertiesService.getScriptProperties().getProperty('wait_tracker_url');
+//     const options = {
+//         method: "post",
+//         contentType: "application/json",
+//         payload: JSON.stringify(waitData)
+//     };
+//     const response = UrlFetchApp.fetch(url, options);
+//     const content = response.getContentText();
+//     console.log(content);
+// }
