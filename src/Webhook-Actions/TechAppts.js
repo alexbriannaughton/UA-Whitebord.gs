93145f099f913461f77f1c4c918c1db378e4f696
Alex Brian Naughton <alexbriannaughton@gmail.com>
2024-06-20 13:20:53 -0700

    update

--- src/Webhook-Actions/TechAppts.js
+++ src/Webhook-Actions/TechAppts.js
@@ -1,10 +1,12 @@
 function addTechAppt(appointment) {
   const location = whichLocation(appointment.resources[0].id);
+  if (location === 'DT') return;
+
   const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(location);
 
   const techBoxCoordsMap = new Map([
     ['CH', 'K6:O21'],
-    ['DT', 'L3:N11'],
+    // ['DT', 'L3:N11'],
     ['WC', 'K4:M13']
     // we are intentionally leaving out the 'in ezyvet' checkbox column bc we dont care if it's true or false to add a patient here
   ]);
@@ -16,22 +18,54 @@ function addTechAppt(appointment) {
 
   const [animalName, animalSpecies] = getAnimalInfo(appointment.animal_id);
 
-  const mainCell = rowRange.offset(0, 1, 1, 1);
+  // const mainCell = rowRange.offset(0, 1, 1, 1);
+
+
+  const timeText = convertEpochToUserTimezone(appointment.modified_at);
+  const timeRichText = simpleTextToRichText(timeText);
 
   // populate main cell: name, species, reason... and make it a link
   const text = `${animalName} (${animalSpecies}) - ${appointment.description}`;
   const webAddress = `${sitePrefix}/?recordclass=Consult&recordid=${appointment.consult_id}`;
   const link = makeLink(text, webAddress);
-  mainCell.setRichTextValue(link);
+  // mainCell.setRichTextValue(link);
 
   // find column to left of mainCell and add time
-  mainCell.offset(0, -1, 1, 1).setValue(
-    convertEpochToUserTimezone(appointment.modified_at)
-  );
+  // mainCell.offset(0, -1, 1, 1).setValue(
+  //   convertEpochToUserTimezone(appointment.modified_at)
+  // );
 
   // check the ezyVet checkbox
-  const checkboxCell = rowRange.offset(0, rowRange.getNumColumns(), 1, 1);
-  checkboxCell.setDataValidation(createCheckbox()).setValue(true);
+  // const checkboxCell = rowRange.offset(0, rowRange.getNumColumns(), 1, 1);
+  // checkboxCell.setDataValidation(createCheckbox()).setValue(true);
+  let richTextVals;
+  if (location === 'CH') {
+    richTextVals = [
+      [
+        timeRichText,
+        link,
+        link,
+        link
+      ]
+    ]
+  }
+
+  else if (location === 'WC') {
+    richTextVals = [
+      [
+        timeRichText,
+        link,
+        simpleTextToRichText(''),
+        simpleTextToRichText('TRUE')
+      ]
+    ]
+  }
+
+  if (!richTextVals) {
+    throw new Error(`No rich text vals at end of addTechAppt(): ${appointment}`);
+  }
+  
+  rowRange.offset(0, 0, 1, 4).setRichTextValues(richTextVals);
 
   return;
 
