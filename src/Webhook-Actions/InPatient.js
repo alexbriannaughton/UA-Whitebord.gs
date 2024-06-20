4bdb1f3b8018aa208975f70ac6962758b043d6ee
Alex Brian Naughton <alexbriannaughton@gmail.com>
2024-06-20 11:02:23 -0700

    set rich text in batch call

--- src/Webhook-Actions/InPatient.js
+++ src/Webhook-Actions/InPatient.js
@@ -5,21 +5,25 @@ function addInPatient(appointment) {
   const inpatientBoxRange = sheet.getRange(
     inpatientBoxCoords(location)
   );
-  const { highestEmptyRow: rowRange } = findRow(inpatientBoxRange, appointment.consult_id, 0);
-  if (!rowRange) return;
-  rowRange.setBackground(inpatientDefaultColorMap.get(location));
-  populateInpatientRow(appointment, rowRange);
+  const { highestEmptyRow } = findRow(inpatientBoxRange, appointment.consult_id, 0);
+  if (!highestEmptyRow) return;
+  highestEmptyRow.setBackground(inpatientDefaultColorMap.get(location));
+  populateInpatientRow(appointment, highestEmptyRow);
   return;
 };
 
-function populateInpatientRow(appointment, rowRange) {
+function populateInpatientRow(appointment, highestEmptyRow) {
   const [animalName, animalSpecies, contactLastName] = getAnimalInfoAndLastName(appointment.animal_id, appointment.contact_id);
-  const nameCell = rowRange.offset(0, 0, 1, 1);
+  // const nameCell = highestEmptyRow.offset(0, 0, 1, 1);
   const text = `${animalName} ${contactLastName} (${animalSpecies})`;
   const webAddress = `${sitePrefix}/?recordclass=Consult&recordid=${appointment.consult_id}`;
   const link = makeLink(text, webAddress);
-  nameCell.setRichTextValue(link);
-  const reasonCell = rowRange.offset(0, 3, 1, 1);
-  reasonCell.setValue(appointment.description);
+  // nameCell.setRichTextValue(link);
+  // const reasonCell = highestEmptyRow.offset(0, 3, 1, 1);
+  // reasonCell.setValue(appointment.description);
+  const emptyRichText = simpleTextToRichText('');
+  highestEmptyRow.offset(0, 0, 1, 4).setRichTextValues([
+    [link, link, emptyRichText, appointment.description]
+  ]);
   return;
 };
