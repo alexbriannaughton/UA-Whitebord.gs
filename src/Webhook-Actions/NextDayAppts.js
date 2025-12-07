94bd76f772069ca5ff0721932be685f2edfb18be
Alex Brian Naughton <alexbriannaughton@gmail.com>
2025-12-06 23:25:08 -0800

    cont ndas

--- src/Webhook-Actions/NextDayAppts.js
+++ src/Webhook-Actions/NextDayAppts.js
@@ -335,6 +335,7 @@ function getNdaRangeForLoc(sheet, uaLoc) {
     }
 
     const colAValues = sheet.getRange(1, 1, lastRow, 1).getValues();
+    console.log('colavals', colAValues)
     // 1. Find the header row for this location/date
     let headerRow = null;
     for (let i = 0; i < colAValues.length; i++) {
